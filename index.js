7c2cfc0cf4414c6c389ece0847b900093ec096b2
Andrejs Krilovs <andrejs.krilovs.0507@gmail.com>
2020-11-01 23:34:55 +0200

    Configured save data to google spreadsheets

--- index.js
+++ index.js
@@ -6,15 +6,61 @@ const data_parser = body_parser.urlencoded({
     extended: false
 })
 
+const {google} = require('googleapis')
+const keys = require('./keys.json')
+
+const writer = new google.auth.JWT(
+    keys.client_email,
+    null,
+    keys.private_key,
+    ['https://www.googleapis.com/auth/spreadsheets']
+)
+
+let person = {
+    name: '',
+    instagram: '',
+    phone: ''
+}
+
+app.listen(3000)
 app.use(express.static(__dirname + '/public'))
 app.get('/', (request, response) => {
     response.sendFile(__dirname + '/index.html')
 })
 
-app.post('/', data_parser, (request, response) => {
+app.post('/commit', data_parser, (request, response) => {
     if(!request.body) {
         return response.status(400)
+    } else {
+        person.name = request.body.name
+        person.instagram = request.body.instagram
+        person.phone = request.body.phone
+
+        writer.authorize((err, tokens) => {
+            if(err) {
+                console.log(err)
+            } else {
+                save(writer, person).then(console.log(''))
+                response.redirect('https://aks.customer.smartsender.eu/lp/Z909qPE0')
+            }
+        })
     }
 })
 
-app.listen(3000)
+async function save(cl, p) {
+    const spreadsheetApi = google.sheets({version: 'v4', auth: cl})
+
+    const request = {
+        spreadsheetId: keys.spreadsheet_id,
+        range: 'Data!B3',
+        valueInputOption: 'RAW',
+        insertDataOption: 'INSERT_ROWS',
+        resource: {
+            values: [
+                [p.name, p.instagram, p.phone]
+            ]
+        }
+    }
+
+    await spreadsheetApi.spreadsheets.values.append(request)
+}
